be1e8ca255191536a4bae9d46a801f2bf1b9d91c
hung456789a <hung456789a@gmail.com>
2025-05-06 19:53:27 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 // index.js
 const http = require('http');
+const winston = require('winston');
+require('winston-logstash');
+
+const logger = winston.createLogger({
+    transports: [
+      new winston.transports.Logstash({
+        port: 5000,
+        node_name: 'my-node-app',
+        host: 'localhost' // địa chỉ Logstash
+      })
+    ]
+  });
 
 const server = http.createServer((req, res) => {
+  logger.info('Received request', { url: req.url });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, world!\n');
 });
 
 server.listen(3000, () => {
+  logger.info('Server started on port 3000');
   console.log('Server running at http://localhost:3000/');
 });
